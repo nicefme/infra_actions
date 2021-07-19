fed87f4491ae694599341796040d25452e20b2c8
nicefme <turutov.a.s@yandex.ru>
2021-07-19 22:54:31 +0400

    v1.0

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,13 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('У меня получилось!')
-   # text = 'У меня получилось!'
-   # return render(request, 'first_page.html', {'text': text})
 
 def second_page(request):
     return HttpResponse('А это вторая страница!')
-   # text = 'А это вторая страница'
-   # return render(request, 'second_page.html', {'text': text})
